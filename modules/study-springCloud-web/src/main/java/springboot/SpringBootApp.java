c7b7cc9ff1bc1851a3fccee12fcc4be7896b844b
guo.wangheng <guowangheng@163.com>
2018-08-01 13:49:59 +0800

    20180801

--- modules/study-springCloud-web/src/main/java/springboot/SpringBootApp.java
+++ modules/study-springCloud-web/src/main/java/springboot/SpringBootApp.java
@@ -2,12 +2,9 @@ package springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @SpringBootApplication  //相当于Configuration + EnableAutoConfiguration + ComponentScan
